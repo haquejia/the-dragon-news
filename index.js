a549d5b3f865fe490e6ed2d0a21807b90eead8b4
haquejia <hemaljia0017@gmail.com>
2023-07-27 17:30:30 +0600

    vercel config added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const catagories = require('./data/catagories.json')
+const news = require('./data/news.json')
+
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -14,6 +16,30 @@ app.get('/catagories', (req, res) => {
     res.send(catagories)
 });
 
+app.get('/news', (req, res) => {
+    res.send(news)
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+
+});
+
+app.get('/catagories/:id', (req, res) => {
+    const id =parseInt( req.params.id);
+    if (id == 0) {
+        res.send(news)
+    }
+    else {
+
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
+
+});
+
 app.listen(port, () => {
-    console.log(`dragone api is running on port:${port}`)
+    console.log(`dragon api is running on port:${port}`)
 })
